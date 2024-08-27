cd656af5178cae4d47f9ceb14de55a7bd88dac35
jsalicetti <jessicasalicetti@yahoo.com>
2024-08-27 15:35:32 -0400

    complete

---
+++ lab/src/main/java/com/g/lab/controllerstwo/MyControllertwo.java
@@ -0,0 +1,12 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyControllertwo{
+    @GetMapping({ "login"})   // either type '/' or index
+    public String showIndex()
+    {
+        return "index";
+    }
+}
+
